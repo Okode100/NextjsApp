23aa9b7f2db2639390db6ce41b4ccdb482873d68
Okode100 <alfakings73@gmail.com>
2025-10-07 23:47:43 +0300

    create a formdata that can fetchdata from the server

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,5 +1,12 @@
 'use server';
 export async function createInvoice(formData: FormData){
+    const rawFormData = {
+        customersName: formData.get('customersName'),
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    }
+     console.log(rawFormData);
 
 
 }
