374cb845cfb0869adbb7be7a401d0675f8ec3d42
Okode100 <alfakings73@gmail.com>
2025-10-08 23:50:52 +0300

    revalidated data to reduce server client time

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -2,6 +2,7 @@
 
 import { z } from 'zod';
 import postgres from 'postgres';
+import { revalidatePath } from 'next/cache';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -24,7 +25,7 @@ export async function createInvoice(formData: FormData){
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-    
+    revalidatePath('/dashboard/invoices');
     console.log({customerId, amountInCents, status, date});
 
 
