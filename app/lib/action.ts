2f6f675e0832c476e38a9981acff3bfdd0a9057b
Okode100 <alfakings73@gmail.com>
2025-10-14 23:27:02 +0300

    create AT self validate

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -17,7 +17,7 @@ const FormSchema = z.object({
    
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
   const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-export async function createInvoice(formData: FormData){
+export async function createInvoice(prevState: any, formData: FormData){
     const {customerId, amount, status} = CreateInvoice.parse({
         customerId: formData.get('customerId') || '',
         amount: formData.get('amount') || '0',
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -8,10 +8,14 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import {createInvoice} from '@/app/lib/action'
+import { useActionState } from 'react';
+
+const initialState = { message: null, errors: {} } as any;bbbbbb
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const [state, formAction] = useActionState(createInvoice, initialState);
   return (
-    <form action = {createInvoice}>
+    <form action = {formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -72,6 +76,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                 
                 />
                 <label
                   htmlFor="pending"
